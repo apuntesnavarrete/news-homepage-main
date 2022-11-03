055c280d7c85dd1b792cfcd6badb5dca793d3412
apuntesnavarrete <apuntesnavarrete@gmail.com>
2022-11-03 17:17:48 -0600

    movil version arreglada

---
+++ js/index.js
@@ -0,0 +1,23 @@
+let buttonOpen = document.getElementById("button_menu")
+let buttonClose = document.getElementById("menu_close")
+
+let menu = document.getElementById("menu")
+
+
+buttonClose.addEventListener("click", menuClose);
+
+buttonOpen.addEventListener("click", menuOPen);
+
+function menuOPen(){
+    console.log("click de apertura")
+    menu.style.transition = "all 1s"  
+
+    menu.style.top = "0"  
+
+}
+
+function menuClose(){
+    console.log("click de cierre")
+    menu.style.top = "-100%"  
+
+}
