2ccab974484034357ec63985b5bc7d41b2867c91
apuntesnavarrete <apuntesnavarrete@gmail.com>
2022-11-04 14:36:07 -0600

    identar y correcciones

--- js/index.js
+++ js/index.js
@@ -1,23 +1,20 @@
-let buttonOpen = document.getElementById("button_menu")
-let buttonClose = document.getElementById("menu_close")
+let buttonOpen = document.getElementById("button_menu");
+let buttonClose = document.getElementById("menu_close");
 
-let menu = document.getElementById("menu")
-
+let menu = document.getElementById("menu");
 
 buttonClose.addEventListener("click", menuClose);
 
 buttonOpen.addEventListener("click", menuOPen);
 
-function menuOPen(){
-    console.log("click de apertura")
-    menu.style.transition = "all 1s"  
+function menuOPen() {
+  console.log("click de apertura");
+  menu.style.transition = "all 1s";
 
-    menu.style.top = "0"  
-
+  menu.style.top = "0";
 }
 
-function menuClose(){
-    console.log("click de cierre")
-    menu.style.top = "-100%"  
-
+function menuClose() {
+  console.log("click de cierre");
+  menu.style.top = "-100%";
 }
